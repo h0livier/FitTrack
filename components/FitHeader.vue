9f51cb6ea56a674a28fd3880eea3a76bfc2ba09b
Hayot Olivier <olivier.hayot.dev@gmail.com>
2025-12-14 17:56:08 +0100

    added cog icon

--- components/FitHeader.vue
+++ components/FitHeader.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 </script>
 <template>
   <div class="navbar bg-base-200 rounded-lg p-4">
@@ -6,7 +8,9 @@
       <NuxtLink to="/" class="btn btn-ghost normal-case text-xl">FitTrack</NuxtLink>
     </div>
     <div class="flex-none gap-2">
-      <NuxtLink to="/settings" class="btn btn-ghost">Paramètres</NuxtLink>
+      <NuxtLink to="/settings" class="btn btn-ghost flex items-center gap-2">
+        <FontAwesomeIcon :icon="faCog" />
+      </NuxtLink>
       <!--
       <NuxtLink to="/weights" class="btn btn-ghost">Weights</NuxtLink>
       <NuxtLink to="/activities" class="btn btn-ghost">Activities</NuxtLink>
