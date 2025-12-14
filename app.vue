a68539d89f860d0f2bac7dfd863b1cd1f008be49
Hayot Olivier <olivier.hayot.dev@gmail.com>
2025-12-14 16:16:51 +0100

    added manifest in head

--- app.vue
+++ app.vue
@@ -4,6 +4,9 @@ useHead({
   meta: [
     { name: 'description', content: "Simple Web App permettant le suivi du poids." }
   ],
+  link: [
+    { rel: 'manifest', href: '/manifest.json' }
+  ],
 })
 </script>
 <template>
